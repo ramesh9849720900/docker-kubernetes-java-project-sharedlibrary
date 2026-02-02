b8ec29412c7fec3de8b9173dee926cd27bde4c0b
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2026-02-03 05:25:06 +0530

    Update k8sDeployCanary.groovy

--- vars/k8sDeployCanary.groovy
+++ vars/k8sDeployCanary.groovy
@@ -1,1 +1,8 @@
 
+def call(String imageTag) {
+    sh """
+    kubectl apply -f kuberenetes/stockmanager-canary.yaml
+    kubectl set image deployment/stockmanager-canary \
+      stockmanager=${ECR_REPO}:${imageTag}
+    """
+}
