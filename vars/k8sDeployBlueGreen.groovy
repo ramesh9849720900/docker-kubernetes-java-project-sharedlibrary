02c72560ad5ea83085e5cef095601da86cf8672f
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2026-02-03 05:24:35 +0530

    Update k8sDeployBlueGreen.groovy

--- vars/k8sDeployBlueGreen.groovy
+++ vars/k8sDeployBlueGreen.groovy
@@ -1,1 +1,8 @@
 
+def call(String color, String imageTag) {
+    sh """
+    kubectl apply -f kubernetes/stockmanager-${color}.yaml
+    kubectl set image deployment/stockmanager-${color} \
+      stockmanager=${ECR_REPO}:${imageTag}
+    """
+}
