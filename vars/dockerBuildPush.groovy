5d48440d6dc2d73bb6ad53855af4f4af43ac1709
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2026-02-03 05:22:55 +0530

    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,1 +1,11 @@
+def call(String imageTag) {
+    sh """
+      aws ecr get-login-password --region us-east-1 \
+      | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.eu-north-1.amazonaws.com
+
+      docker build -t stockmanager:${imageTag} .
+      docker tag stockmanager:${imageTag} ${ECR_REPO}:${imageTag}
+      docker push ${ECR_REPO}:${imageTag}
+    """
+}
 
